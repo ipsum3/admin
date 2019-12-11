d0f996d31ca7b071683b4cae3a5326ca2b0834b6
B1nj <postmaster@b1nj.fr>
2019-12-11 12:07:48 -0400

    modification gestion erreur

--- src/app/Http/Requests/FormRequest.php
+++ src/app/Http/Requests/FormRequest.php
@@ -8,12 +8,4 @@ use Prologue\Alerts\Facades\Alert;
 class FormRequest extends HttpFormRequest
 {
 
-    public function withValidator($validator)
-    {
-        $validator->after(function ($validator) {
-            if ($validator->failed()) {
-                Alert::warning($validator->getMessageBag()->count() > 1 ? __("Il y a :count erreurs sur ce formulaire que vous devez corriger", ['count' => $validator->getMessageBag()->count()]) : __("Il y a une erreur sur ce formulaire que vous devez corriger"))->flash();
-            }
-        });
-    }
 }
--- src/ressources/views/partials/alert.blade.php
+++ src/ressources/views/partials/alert.blade.php
@@ -30,3 +30,19 @@
         </div>
     @endforeach
 @endforeach
+
+@if ($errors->any())
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <div class="alert-icon">
+            <i class="fas fa-exclamation-triangle"></i>
+        </div>
+        <div class="alert-text">
+            {{ $errors->count() > 1 ? __("Il y a :count erreurs sur ce formulaire que vous devez corriger", ['count' => $errors->count()]) : __("Il y a une erreur sur ce formulaire que vous devez corriger") }}.
+            {{--<ul class="li">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>--}}
+        </div>
+    </div>
+@endif

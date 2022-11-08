eee037b02ea28ed6f86f5780fd8ffb75349f9485
PANOR Ismael <i.panor@pixell.eu>
2022-11-08 14:36:29 -0400

    Fix encodage html

--- src/ressources/views/components/custom.blade.php
+++ src/ressources/views/components/custom.blade.php
@@ -2,26 +2,26 @@
 
 @switch($type)
     @case('checkbox')
-    <input type="hidden" name="{{ $name }}" value="0">
-    {{ Aire::checkbox($name, $label)->value(1)->checked(e( $value ))->helpText((string) $description) }}
-    @break
+        <input type="hidden" name="{{ $name }}" value="0">
+        {{ Aire::checkbox($name, $label)->value(1)->checked( $value )->helpText((string) $description) }}
+        @break
 
     @case('select')
-    {{ Aire::select($options, $name, $label)->value(e( $value ) )->helpText((string) $description) }}
-    @break
+        {{ Aire::select($options, $name, $label)->value( $value )->helpText((string) $description) }}
+        @break
 
     @case('radio')
-    {{ Aire::radioGroup($options, $name, $label)->value( e($value) )->helpText((string) $description) }}
-    @break
+        {{ Aire::radioGroup($options, $name, $label)->value( $value )->helpText((string) $description) }}
+        @break
 
     @case('html-simple')
         {{ Aire::textArea($name, $label)->class('tinymce-simple')->value($value)->helpText((string) $description) }}
-    @break
+        @break
 
     @case('html')
         {{ Aire::textArea($name, $label)->class('tinymce')->value($value)->helpText((string) $description) }}
-    @break
+        @break
 
     @default
-    {{ Aire::{$type}($name, $label)->value(e( $value ))->helpText((string) $description) }}
+        {{ Aire::{$type}($name, $label)->value( $value )->helpText((string) $description) }}
 @endswitch
